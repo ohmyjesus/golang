d705d2400d7b264688fde78ddf2b3abcc555eab4
iHoney725 <60086881+ohmyjesus@users.noreply.github.com>
2021-05-20 22:21:11 +0800

    map的多线程安全版本sync_map

---
+++ day08/main3.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+	"sync"
+)
+
+// Go内置的map不是并发安全的
+var m = make(map[string]int)
+
+func get(key string) int {
+	return m[key]
+}
+
+func set(key string, value int) {
+	m[key] = value
+}
+
+// func main() {
+// 	wg := sync.WaitGroup{}
+// 	for i := 0; i < 20; i++ {
+// 		wg.Add(1)
+// 		go func(n int) {
+// 			key := strconv.Itoa(n)
+// 			set(key, n)
+// 			fmt.Printf("k=:%v,v:=%v\n", key, get(key))
+// 			wg.Done()
+// 		}(i)
+// 	}
+// 	wg.Wait()
+// }
+
+var m2 sync.Map
+
+// 多线程使用map时需要使用安全的sync.map
+func main() {
+	wg := sync.WaitGroup{}
+	for i := 0; i < 20; i++ {
+		wg.Add(1)
+		go func(n int) {
+			key := strconv.Itoa(n)
+			m2.Store(key, n) //必须使用 sync.map 的内置方法
+			value, _ := m2.Load(key)
+			fmt.Printf("k=:%v,v:=%v\n", key, value)
+			wg.Done()
+		}(i)
+	}
+	wg.Wait()
+}
